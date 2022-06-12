a1820a3a7b8082f2b4db460f6df4cc48e90c6757
syahrilasidiq555 <syahrilasidiq555@gmail.com>
2022-06-12 21:03:01 +0700

    finish on minute 1:38:37

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,48 @@
+import { useState } from 'react';
+import Video from '../../videos/video.mp4';
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements';
+
+import { Button } from '../ButtonElement';
+
 const HeroSection = () => {
+    const [ hover, setHover ] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
+
     return (
         <HeroContainer id="home">
             <HeroBg>
-                <VideBg autoPlay loop muted src={Video} type='video/mp4' />
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Mode Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive $250 in credit towards your next Payment.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" 
+                        onMouseEnter={onHover} 
+                        onMouseLeave={onHover}
+                        primary="true"
+                        dark="true"    
+                    >
+                        Get started {hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     );
 }
