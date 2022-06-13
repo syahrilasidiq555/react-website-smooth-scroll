fb59815376bf6ae1446b2aadbd5ce369d444d0fa
unknown <syahril.firdaus@dwidasa.com>
2022-06-13 13:47:44 +0700

    change hero section text

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -28,9 +28,16 @@ const HeroSection = () => {
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
             <HeroContent>
-                <HeroH1>Virtual Banking Mode Easy</HeroH1>
+                {/* <HeroH1>Virtual Banking Mode Easy</HeroH1>
                 <HeroP>
                     Sign up for a new account today and receive $250 in credit towards your next Payment.
+                </HeroP> */}
+                <HeroH1>Do You Know What is Cunt? Yea That's You</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive $250 in credit towards your next Payment.
+                </HeroP>
+                <HeroP style={{fontSize:'5px'}}>
+                    I Lied.
                 </HeroP>
                 <HeroBtnWrapper>
                     <Button to="signup" 
