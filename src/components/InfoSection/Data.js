8b2da6e582bfade240e01b625db7a1dd8ad503a7
unknown <syahril.firdaus@dwidasa.com>
2022-06-14 09:54:03 +0700

    finishing on minute 2:31:51

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,54 @@
+import gambar1 from '../../images/svg-1.svg';
+import gambar2 from '../../images/svg-2.svg';
+import gambar3 from '../../images/svg-3.svg';
+
+export const homeObjOne = {
+    id : 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charget any fees',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: gambar2,
+    alt: 'Cars',
+    dark: false,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id : 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charget any fees',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: gambar1,
+    alt: 'img',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id : 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: gambar3,
+    alt: 'Cars',
+    dark: false,
+    primary: false,
+    darkText: true
+}
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { Link as LinkScroll } from 'react-scroll';
 export const Nav = styled.nav`
     background: #000;
     height: 80px;
-    // margin-top: -80px;
+    /* margin-top: -80px; */
     display: flex;
     justify-content: center;
     align-items: center;
@@ -13,7 +13,6 @@ export const Nav = styled.nav`
     position: sticky;
     top: 0;
     z-index: 10;
-
 
     @media screen and (max-width: 960px) {
         transition: 0.8s all ease;
